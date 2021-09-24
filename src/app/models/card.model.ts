0f694848a7e226c8f5d50677d2ea7f4589cfcafd
utkarsh2609 <utkarsh.awasthi01@gmail.com>
2021-09-25 00:43:32 +0530

    removed commented code

--- src/app/models/card.model.ts
+++ src/app/models/card.model.ts
@@ -1,6 +1,5 @@
 export class CreditDebitCardModel {
     cardNumber: string | undefined;
-    // cardType: string | undefined;
     cardCVV: number | undefined;
     cardExpiryDate: Date | undefined;
 }
